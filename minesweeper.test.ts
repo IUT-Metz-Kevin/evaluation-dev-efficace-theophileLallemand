8ad60eb94cf8e9160c5a278c88416f1aefede000
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-06-03 14:33:28 +0200

    GREEN pour . = 0, RED .*=1

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -3,16 +3,18 @@ import { expect } from "jsr:@std/expect";
 
 function MineSweeper(tableauMiné: string): string {
    for (let i = 0; i === tableauMiné.length; i++) {
-    if (tableauMiné[i+1] == "*") return "1";
+    if (tableauMiné[i + 1] == "*") return "1";
    }
   return "0"
 }
 
 describe("test pour nombre de mine", () => {
-
-
+  it("zero mine", () => {
+     expect(MineSweeper(".")).toBe("0");
+  });
+  
     it("1 mine", () => {
-     expect(MineSweeper(".*")).toBe("1");
+     expect(MineSweeper("*")).toBe("1");
   });
 
 });
