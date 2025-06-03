ce086fc22c3e667169b3d86c9fc7ceb275f82a4d
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-06-03 15:08:22 +0200

    REFACTOR ajout pour .* = 1 et ajout boucle

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,22 +1,32 @@
 import { it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
 
+
 function MineSweeper(tableauMiné: string): string {
-    if (tableauMiné[1] == "*") return "1";
-    if(tableauMiné[0] == "*" && tableauMiné[2] == "*") return "2";
+  for (let i = 0; i < tableauMiné.length; i++) {
+    if (tableauMiné[i+1] == "*") return "1";
+    if(tableauMiné[i-1] == "*" && tableauMiné[i+1] == "*") return "2";
+    if (tableauMiné[i-1] == "*") return "1";
     else return "0"
    }
+   return ""
+  }
 
   it("zero mine", () => {
      expect(MineSweeper(".")).toBe("0");
   });
   
-    it("1 mine", () => {
+    it("1 mine après", () => {
      expect(MineSweeper(".*")).toBe("1");
 
   });
 
-    it("2 mines (une de chaque c", () => {
+      it("1 mine avant", () => {
+     expect(MineSweeper("*.")).toBe("1");
+
+  });
+
+    it("2 mines (une de chaque coté", () => {
      expect(MineSweeper("*.*")).toBe("2");
 
   });
