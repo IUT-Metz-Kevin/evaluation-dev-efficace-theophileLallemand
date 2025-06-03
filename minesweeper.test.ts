860a6676087323f205519966d34a9b32745f4c0f
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-06-03 15:42:29 +0200

    GREEN pour . = 0 et  .*=1, *.*= 2, *. =1, ......\n* = 1

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,32 +1,31 @@
 import { it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
 
-
 function MineSweeper(tableauMiné: string): string {
   for (let i = 0; i < tableauMiné.length; i++) {
-    if(tableauMiné[i-1] == "*" && tableauMiné[i+1] == "*") return "2";
-    if (tableauMiné[i-1] == "*") return "1";
-    if (tableauMiné[i+1] == "*") return "1";
-   }
-   return "0"
+    if (tableauMiné[i - 1] == "*" && tableauMiné[i + 1] == "*") return "2";
+    if (tableauMiné[i + 8] == "*") return "1";
+    if (tableauMiné[i - 1] == "*") return "1";
+    if (tableauMiné[i + 1] == "*") return "1";
   }
-  
-  it("zero mine", () => {
-     expect(MineSweeper(".")).toBe("0");
-  });
-  
-    it("1 mine après", () => {
-     expect(MineSweeper(".*")).toBe("1");
+  return "0";
+}
 
-  });
-  
-      it("1 mine avant", () => {
-     expect(MineSweeper("*.")).toBe("1");
+it("zero mine", () => {
+  expect(MineSweeper(".")).toBe("0");
+});
 
-  });
+it("1 mine après", () => {
+  expect(MineSweeper(".*")).toBe("1");
+});
 
-    it("2 mines (une de chaque coté", () => {
-     expect(MineSweeper("*.*")).toBe("2");
+it("1 mine avant", () => {
+  expect(MineSweeper("*.")).toBe("1");
+});
+it("1 mine en dessous", () => {
+  expect(MineSweeper("......\n*")).toBe("1");
+});
 
-  });
-
+it("2 mines (une de chaque coté", () => {
+  expect(MineSweeper("*.*")).toBe("2");
+});
