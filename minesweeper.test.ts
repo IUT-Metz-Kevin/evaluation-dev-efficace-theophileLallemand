ccc66015a91bcd9bcfc72e21b8df537e3e100515
LALLEMAND Theophile <theophile.lallemand9@etu.univ-lorraine.fr>
2025-06-03 14:27:45 +0200

    REFACTOR pour ajout de boucle for pour * return  1 et .  return 0

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -2,11 +2,10 @@ import { describe, it } from "jsr:@std/testing/bdd";
 import { expect } from "jsr:@std/expect";
 
 function MineSweeper(tableauMiné: string): string {
-  // for (let i = 0; i === tableauMiné.length; i++) {
-  //   if (tableauMiné[i + 1] == "*") return "1";
-  // }
-  // return "0"
-  return "";
+   for (let i = 0; i === tableauMiné.length; i++) {
+    if (tableauMiné[i + 1] == "*") return "1";
+   }
+  return "0"
 }
 
 describe("add function", () => {
